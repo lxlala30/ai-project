6ef034f5c4773951ff50b84c25919ce1e248ba3c
lixin <1923035819@qq.com>
2026-01-15 09:58:23 +0800

    增加脚本一键启动以及项目功能说明readme.md文件

---
+++ run_py_v1.bat
@@ -0,0 +1,29 @@
+@echo off
+REM 进入指定目录
+cd /d "D:\lx\2026\ai-project"
+
+REM 检查是否成功进入目录
+if %errorlevel% neq 0 (
+    echo 无法进入指定目录: D:\lx\2026\ai-project
+    pause
+    exit /b 1
+)
+
+:loop
+REM 调用 Python 脚本 main.py
+echo 正在调用 Python 脚本 py-ai1.0.py...
+python .\py-xiaozhi\py-ai1.0.py
+
+REM 等待指令执行完成
+echo Python 脚本 py-ai1.0.py 执行完成。
+
+REM 提示用户是否重新执行脚本
+echo.
+echo 按回车键重新执行脚本，或按任意其他键退出...
+set /p "="
+if errorlevel 255 goto :eof
+goto :loop
+
+:eof
+echo 脚本已退出。
+pause
